d473c982a64402e47a9e060ff80cd1c23f2b90d8
TheJoboReal <kasper.jobo@gmail.com>
2025-02-27 17:50:18 +0100

    forfra

--- freertos-pi-pico/freertos-pi-pico/app/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/kasper/Documents/Civilingenioer_Robotteknologi/ISA_IPC/freertos-pi-pico/freertos-pi-pico/app/_deps/picotool/"
-};
